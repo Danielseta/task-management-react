6d33b41b3a449a33601c577391a6237472d614cd
Daniel seta <danielseta37@gmail.com>
2024-12-10 23:03:32 -0800

    AddEditBoardModal

--- src/modals/AddEditBoardModal.jsx
+++ src/modals/AddEditBoardModal.jsx
@@ -2,7 +2,14 @@ import React from 'react'
 
 const AddEditBoardModal = () => {
   return (
-    <div>AddEditBoardModal</div>
+    <div 
+    onClick={(e) => {
+        
+    }}
+    className=' fixed right-0 left-0 top-0 bottom-8 px-2 py-4 overflow-scroll z-50 justify-center items-center flex bg-[#000000]'
+    >
+
+    </div>
   )
 }
 
