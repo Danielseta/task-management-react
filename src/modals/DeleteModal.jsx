be60140e83613e3155e8dbbce1ee5482db785ccc
Daniel seta <danielseta37@gmail.com>
2024-12-24 23:26:39 -0800

    feat: update DeleteModal component structure and add props for customization

--- src/modals/DeleteModal.jsx
+++ src/modals/DeleteModal.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
-function DeleteModal() {
-  return <div>DeleteModal</div>;
+function DeleteModal({ type, title, onDeleteBtnClick }) {
+  return;
+  //contianer
+  <div className=" fixed right-0 bottom-0 left-0 top-0 pc-2 py-4 overflow-scroll scrollbar-hide z-50 justify-center items-center fflex bg-[#00000080]">
+    {/* Delete Modal*/}
+  </div>;
 }
 
 export default DeleteModal;
