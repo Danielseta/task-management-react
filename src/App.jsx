1b0c2b96467d71e94c6a1892dbd39579570cf9b4
Daniel seta <danielseta37@gmail.com>
2024-12-27 20:59:30 -0800

    feat: integrate Redux for board state management and set active board on load

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,35 @@
-import React, { useState } from 'react'
-import Header from './componets/Header'
-import Center from './componets/Center'
+import React, { useState } from "react";
+import Header from "./componets/Header";
+import Center from "./componets/Center";
+import { useDispatch, useSelector } from "react-redux";
+import boardsSlice from "./redux/boardsSlice";
 
-const App = () => {
-   
-  const [boardModalOpen, setBoardModalOpen] = useState(false)
+function App() {
+  const dispatch = useDispatch();
+
+  const boards = useSelector((state) => state.boards);
+  const activeBoard = boards.find((board) => board.isActive);
+
+  if (!activeBoard && boards.length > 0) {
+    dispatch(boardsSlice.actions.setBoardActive({ index: 0 }));
+  }
+
+  const [boardModalOpen, setBoardModalOpen] = useState(false);
 
   return (
     <div>
-      
-    {/* header */}
+      {/* header */}
 
-    <Header boardModalOpen = {boardModalOpen} setBoardModalOpen={setBoardModalOpen} />
+      <Header
+        boardModalOpen={boardModalOpen}
+        setBoardModalOpen={setBoardModalOpen}
+      />
 
-    {/* Center */}
+      {/* Center */}
 
-    <Center/> 
-
-
+      <Center />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
