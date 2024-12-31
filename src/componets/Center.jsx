3e35264f04fd7fdd554d306c4f7c97416b0ea56d
Daniel seta <danielseta37@gmail.com>
2024-12-31 10:08:39 -0800

    implement SideBar component and enhance window resize handling in Center component

--- src/componets/Center.jsx
+++ src/componets/Center.jsx
@@ -1,34 +1,36 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from "react";
 
-function Center({ boardModalOpen , setBoardModalOpen}) {
-  const [windowSize, setWindowSize] = useState(
-    [
-      window.innerWidth,
-      window.innerHeight
-    ]
-  )
+function Center({ boardModalOpen, setBoardModalOpen }) {
+  const [windowSize, setWindowSize] = useState([
+    window.innerWidth,
+    window.innerHeight,
+  ]);
+
+  const [isSideBarOpen, setIsSideBarOpen] = useState(true);
 
   useEffect(() => {
-    const handleWindow.Resize = () => {
-      setWindowSize([window.innerWidth , window.innerHeight])
-    }
+    const handleWindowResize = () => {
+      setWindowSize([window.innerWidth, window.innerHeight]);
+    };
 
-    window.removeEventListener("resize , handleWindowResize")
+    window.removeEventListener("resize , handleWindowResize");
 
     return () => {
-      window.removeEventListener("resize", handleWindowResize)
-    }
-  })
+      window.removeEventListener("resize", handleWindowResize);
+    };
+  });
 
   return (
     <div
-    className={
-      windowSize[0] >= 768 && isSideBarOpen
-    }
+      className={
+        windowSize[0] >= 768 && isSideBarOpen
+          ? " bg-[#f4f7fd] scrollbar-hide h-screen flex dark:bg-[#20212c] overflow-x-scroll gap-6 ml-[261px]"
+          : "bg-[#f4f7fd] scrollbar-hide h-screen flex dark:bg-[#20212c] overflow-x-scroll gap-6 "
+      }
     >
-
+      {windowSize[0] >= 768 && <SideBar />}
     </div>
-  )
+  );
 }
 
-export default Center
+export default Center;
