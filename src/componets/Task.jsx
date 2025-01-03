553eeea74d4389fb0729ec452811e4ab52b44755
Daniel seta <danielseta37@gmail.com>
2025-01-02 22:49:18 -0800

    task retrieval in Task component

--- src/componets/Task.jsx
+++ src/componets/Task.jsx
@@ -1,6 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Task({ taskIndex, colIndex }) {
+  const boards = useSelector((state) => state.boards);
+  const board = boards.find((board) => board.isActive);
+  const columns = board.columns;
+  const col = columns.find((col, i) => i === colIndex);
+  const task = col.tasks.find((task, i) => i === taskIndex);
+
+  const [isTaskModalOpen, setIsTaskModalOpen] = useState(false);
+
   return <div>Task</div>;
 }
 
