43d0a06452a240f61c087ec1fcecd10f4f3e90db
Daniel seta <danielseta37@gmail.com>
2025-01-03 21:06:48 -0800

    Add task completion display in Task component

--- src/componets/Task.jsx
+++ src/componets/Task.jsx
@@ -10,7 +10,28 @@ function Task({ taskIndex, colIndex }) {
 
   const [isTaskModalOpen, setIsTaskModalOpen] = useState(false);
 
-  return <div>Task</div>;
+  let completed = 0;
+  let subtasks = task.subtasks;
+  subtasks.forEach((subtasks) => {
+    if (subtasks.isCompleted) {
+      completed++;
+    }
+  });
+
+  return (
+    <div>
+      <div
+        className=" w-[280px] first:my-5 rounded-lg bg-white dark:bg-[#2b2c37] shadow-[#364e7e1a]
+    py-6 px-3 shadow-lg hover:text-[#635fc7] dark:text-white dark:hover:text-[#635fc7] cursor-pointer
+    "
+      >
+        <p className=" font-bold tracking-wide">{task.title}</p>
+        <p className=" font-bold text-xs tracking-tighter mt-2 text-gray-500">
+          {completed} of {subtasks.length} completed tasks
+        </p>
+      </div>
+    </div>
+  );
 }
 
 export default Task;
