026a32be2860216bda7d802fccc0a1e04e6fe478
Daniel seta <danielseta37@gmail.com>
2025-01-04 22:39:45 -0800

    error need to Fix variable naming in TaskModal and improve modal styling

--- src/modals/TaskModal.jsx
+++ src/modals/TaskModal.jsx
@@ -4,10 +4,10 @@ import { useDispatch, useSelector } from "react-redux";
 function TaskModal({ colIndex, taskIndex, setIsTaskModalOpen }) {
   const dispatch = useDispatch();
   const boards = useSelector((state) => state.boards);
-  const board = boards.find((board) => board.isActive);
+  const board = boards.find((board) => board.isActive === true);
   const columns = board.columns;
-  const col = columns.find((column, i) => colIndex === i);
-  const task = col.tasks.find((col, i) => taskIndex === i);
+  const col = columns.find((col, i) => i === colIndex);
+  const task = col.tasks.find((task, i) => i === taskIndex);
   const subtasks = task.subtasks;
 
   let completed = 0;
@@ -26,9 +26,13 @@ function TaskModal({ colIndex, taskIndex, setIsTaskModalOpen }) {
     z-50 bottom-0 justify-center items-center flex bg-[#00000080]"
     >
       {/** modal */}
-
+      <div
+        className=" scrollbar-hide overflow-y-scroll max-h-[95vh] my-auto bg-white
+      dark:bg-[#2b2c37] text-black dark:text-white font-bold shadow-md shadow-[364e7e1a] max-w-md mx-auto w-full px-8 py-8 rounded-xl
+      "
+      ></div>
       <div className=" relative flex justify-between w-full items-center">
-        <h1 className=" text-lg">{Text.title}</h1>
+        <h1 className=" text-lg">{text.title}</h1>
         <img />
       </div>
     </div>
