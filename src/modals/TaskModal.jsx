f876e772d1aa58b5f8620355890c8c039efaebca
Daniel seta <danielseta37@gmail.com>
2025-01-07 21:26:51 -0800

    TaskModal component layout and improve styling consistency

--- src/modals/TaskModal.jsx
+++ src/modals/TaskModal.jsx
@@ -26,14 +26,11 @@ function TaskModal({ taskIndex, colIndex, setIsTaskModalOpen }) {
     z-50 bottom-0 justify-center items-center flex bg-[#00000080]"
     >
       {/** modal */}
-      <div
-        className=" scrollbar-hide overflow-y-scroll max-h-[95vh] my-auto bg-white
-      dark:bg-[#2b2c37] text-black dark:text-white font-bold shadow-md shadow-[364e7e1a] max-w-md mx-auto w-full px-8 py-8 rounded-xl
-      "
-      ></div>
-      <div className=" relative flex justify-between w-full items-center">
-        <h1 className=" text-lg">{text.title}</h1>
-        <img />
+      <div className=" scrollbar-hide overflow-y-scroll max-h-[95vh] my-auto bg-white dark:bg-[#2b2c37] text-black dark:text-white font-bold shadow-md shadow-[#364e7e1a] max-w-md mx-auto w-full px-8 py-8 rounded-xl">
+        <div className=" relative flex justify-between w-full items-center">
+          <h1 className=" text lg"></h1>
+          <img />
+        </div>
       </div>
     </div>
   );
