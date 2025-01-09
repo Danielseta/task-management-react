be7a14e262c8b331a41352f53c0cfd173407f993
Daniel seta <danielseta37@gmail.com>
2025-01-09 00:57:05 -0800

    Subtask completion handling and status selection in TaskModal

--- src/componets/Subtask.jsx
+++ src/componets/Subtask.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { useDispatch, useSelector } from "react-redux";
+import boardsSlice from "../redux/boardsSlice";
 
 function Subtask({ index, taskIndex, colIndex }) {
   const dispatch = useDispatch();
@@ -11,12 +12,19 @@ function Subtask({ index, taskIndex, colIndex }) {
   const subtask = task.subtasks.find((subtask, i) => i === index);
   const checked = subtask.isCompleted;
 
+  const onChange = () => {
+    dispatch(
+      boardsSlice.actions.setSubtaskCompleted({ index, taskIndex, colIndex })
+    );
+  };
+
   return (
     <div className=" w-full flex hover:bg-[#635fc740] dark:hover:bg-[#635fc740] rounded-md relative items-center justify-start dark:bg-[#20212c] p-3 gap-4 bg-[#f4f7fd]">
       <input
         type="checkbox"
         className=" w-4 accent-[#635fc7] cursor-pointer"
         checked={checked}
+        onChange={onChange}
       />
       <p className={checked && " line-through opacity-30"}>{subtask.title}</p>
     </div>
--- src/modals/TaskModal.jsx
+++ src/modals/TaskModal.jsx
@@ -71,6 +71,14 @@ function TaskModal({ taskIndex, colIndex, setIsTaskModalOpen }) {
             );
           })}
         </div>
+        {/** status */}
+
+        <div className=" mt-8 flex flex-col space-y-3">
+          <label className=" text-sm dark:text-white text-gray-500">
+            Current Status
+          </label>
+          <select className=" select-status flex-grow px-4 py-2 rounded-md text-sm bg-transparent focus:border-0 border border-gray-300 focus:outline-[#635fc7] outline-none"></select>
+        </div>
       </div>
     </div>
   );
