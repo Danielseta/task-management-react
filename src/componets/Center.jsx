900dfd7aaa4a56c5808c2a2d6c3c95792cc80260
Daniel seta <danielseta37@gmail.com>
2025-01-15 19:51:33 -0800

    Enhance SideBar component with dark mode toggle and board listing functionality; update Center component to pass props to SideBar; ensure Task component supports drag-and-drop.

--- src/componets/Center.jsx
+++ src/componets/Center.jsx
@@ -37,7 +37,12 @@ function Center({ boardModalOpen, setBoardModalOpen }) {
           : "bg-[#f4f7fd] scrollbar-hide h-screen flex dark:bg-[#20212c] overflow-x-scroll gap-6 "
       }
     >
-      {windowSize[0] >= 768 && <SideBar />}
+      {windowSize[0] >= 768 && (
+        <SideBar
+          isSideBarOpen={isSideBarOpen}
+          setIsSideBarOpen={setIsSideBarOpen}
+        />
+      )}
 
       {/** colums  */}
       {columns.length > 0 ? (
